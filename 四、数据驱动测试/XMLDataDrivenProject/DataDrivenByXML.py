ee7cd769d7a80a45681fc6d7e700478c29a9f72f
liqinjia <liqinjia372135@163.com>
2018-04-21 17:38:49 +0800

    V20180421

---
+++ 四、数据驱动测试/XMLDataDrivenProject/DataDrivenByXML.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/4/21 11:32
+# @Author  : Robin Li
+# @Email   : liqinjia372135@163.com
+# @File    : DataDrivenByXML.py
+# @Software: PyCharm
+
+from selenium import webdriver
+import unittest
+import time
+from selenium.common.exceptions import WebDriverException
+import traceback
+
+
+class TestDemo(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+
+    def test_visitSogou(self):
+        url = 'http://www.sogou.com'
+        self.driver.get(url)
+
+    def tearDown(self):
+        self.driver.quit()
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
